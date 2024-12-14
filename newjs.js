5550e108143dfe70cbc513e04520f856db5be8e1
New name <Ei@gami>
2024-12-14 17:16:15 +0100

    somenew

--- newjs.js
+++ newjs.js
@@ -2,3 +2,15 @@ let a = Math.random()
 
 if(a > 0.5)console.log("??")
 else {console.log("!!")}
+
+let newFunction = () => {console.log("something") }
+
+let newfitch = function(){
+    let element = document.createElement("div")
+
+    let parrent = docuent.getElementById("id")
+
+    element.after(parrent)
+  
+
+}
